2ff6371f1aa81bed455c61adbbb881450707dd86
calumroy <calum@calumroy.com>
2021-07-04 16:58:05 +0800

    Added build script and made dynamic shared lib

--- fat_rolling/build.sh
+++ fat_rolling/build.sh
@@ -1,7 +1,10 @@
 
 #!/bin/sh
 # Go to library source tree root and run the usual:
-mkdir build && cd $_
+# mke this an executable with chmod +x ./build.sh
+mkdir install
+mkdir build 
+cd ./build
 cmake ..
 rm -r ./* && cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --target install
